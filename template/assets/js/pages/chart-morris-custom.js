23fcff8c09fc08230ce48ab693fd33cf8fb2e57e
James Bonifacio <jplbonif@uwaterloo.ca>
2022-03-09 18:38:35 -0500

    Scrolling graph git statusls

--- template/assets/js/pages/chart-morris-custom.js
+++ template/assets/js/pages/chart-morris-custom.js
@@ -14,66 +14,105 @@ class Blink {
     }
 }
 
+var VOLTAGE_ON = 3.3
+
 var blinkLevel = 0;
 var blinks = [];
-var risingTimestamp = 0
+var risingTimestamp = 0;
+
+var currTimestamp = 200;
 
 var chartData = [
     {
         timestamp: 0,
         a: 0,
-        b: 0,
+        voltage: 0,
     },
     {
         timestamp: 19,
         a: 0,
-        b: 0
+        voltage: 0
 
     },
     {
         timestamp: 20,
         a: 0,
-        b: 3.3
+        voltage: VOLTAGE_ON
     },
     {
         timestamp: 50,
         a: 0,
-        b: 3.3,
+        voltage: VOLTAGE_ON,
     },
     {
         timestamp: 51,
         a: 0,
-        b: 0,
+        voltage: 0,
     },
     {
         timestamp: 99,
         a: 0,
-        b: 0
+        voltage: 0
     },
     {
         timestamp: 100,
         a: 0,
-        b: 3.3
+        voltage: VOLTAGE_ON
     },
     {
         timestamp: 150,
         a: 0,
-        b: 3.3
+        voltage: VOLTAGE_ON
     },
     {
         timestamp: 151,
         a: 0,
-        b: 0
+        voltage: 0
     },
     {
         timestamp: 200,
         a: 0,
-        b: 0
+        voltage: 0
     },
 ]
 
-function logKey(e) {
+var blinkChart = Morris.Area({
+    element: 'morris-area-chart',
+    data: chartData,
+    xkey: 'timestamp',
+    ykeys: ['a', 'voltage'],
+    labels: ['Series A', 'Series B'],
+    pointSize: 0,
+    fillOpacity: 0.8,
+    pointStrokeColors: ['#b4becb', '#A389D4'],
+    behaveLikeLine: true,
+    gridLineColor: '#e0e0e0',
+    lineWidth: 0,
+    smooth: false,
+    hideHover: 'auto',
+    responsive:true,
+    lineColors: ['#b4becb', '#A389D4'],
+    resize: true
+});
 
+var intervalId = window.setInterval(function(){
+    console.log("Update graph")
+    currTimestamp += 50
+
+    chartData.shift()
+    var currVoltage = blinkLevel == 1 ? VOLTAGE_ON : 0
+    chartData.push({
+        timestamp: currTimestamp,
+        a: 0,
+        voltage: currVoltage
+    })
+
+    blinkChart.setData(chartData)
+}, 1000);
+
+
+
+function logKey(e) {
     // Change in blink level detected
     if (e.keyCode == 98) {
         blinkLevel = 1 - blinkLevel
@@ -92,26 +131,5 @@ function logKey(e) {
 }
 
 
-$(document).ready(function() {
-    setTimeout(function() {
-    Morris.Area({
-        element: 'morris-area-chart',
-        data: chartData,
-        xkey: 'timestamp',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        pointSize: 0,
-        fillOpacity: 0.8,
-        pointStrokeColors: ['#b4becb', '#A389D4'],
-        behaveLikeLine: true,
-        gridLineColor: '#e0e0e0',
-        lineWidth: 0,
-        smooth: false,
-        hideHover: 'auto',
-        responsive:true,
-        lineColors: ['#b4becb', '#A389D4'],
-        resize: true
-    });
-    // [ area-angle-chart ] end
-        }, 700);
-});
+
+
