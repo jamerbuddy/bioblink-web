6994f804a0f9779f53e1ae04ef9980e6df703b11
James Bonifacio <jplbonif@uwaterloo.ca>
2022-03-09 18:11:25 -0500

    Refactor with one notification button

--- template/assets/js/pages/chart-morris-custom.js
+++ template/assets/js/pages/chart-morris-custom.js
@@ -1,86 +1,103 @@
 'use strict';
+
+document.addEventListener('keypress', logKey);
+
+class Blink {
+    constructor(risingEdgeTime, fallingEdgeTime) {
+        // UNIX timestamps (milliseconds since 1980)
+        this.risingEdgeTime = risingEdgeTime
+        this.fallingEdgeTime = fallingEdgeTime
+    }
+
+    calcDuration() {
+        return this.fallingEdgeTime - this.risingEdgeTime
+    }
+}
+
+var blinkLevel = 0;
+var blinks = [];
+var risingTimestamp = 0
+
+var chartData = [
+    {
+        timestamp: 0,
+        a: 0,
+        b: 0,
+    },
+    {
+        timestamp: 19,
+        a: 0,
+        b: 0
+
+    },
+    {
+        timestamp: 20,
+        a: 0,
+        b: 3.3
+    },
+    {
+        timestamp: 50,
+        a: 0,
+        b: 3.3,
+    },
+    {
+        timestamp: 51,
+        a: 0,
+        b: 0,
+    },
+    {
+        timestamp: 99,
+        a: 0,
+        b: 0
+    },
+    {
+        timestamp: 100,
+        a: 0,
+        b: 3.3
+    },
+    {
+        timestamp: 150,
+        a: 0,
+        b: 3.3
+    },
+    {
+        timestamp: 151,
+        a: 0,
+        b: 0
+    },
+    {
+        timestamp: 200,
+        a: 0,
+        b: 0
+    },
+]
+
+function logKey(e) {
+
+    // Change in blink level detected
+    if (e.keyCode == 98) {
+        blinkLevel = 1 - blinkLevel
+        var isRisingEdge = blinkLevel == 1
+        var timestamp = Date.now()
+
+        if (isRisingEdge) {
+            console.log("Rising")
+            risingTimestamp = timestamp;
+        } else {
+            var blink = new Blink(risingTimestamp, timestamp)
+            blinks.push(blink)
+            console.log("Falling, duration: " + blink.calcDuration() + "ms");
+        }   
+    }
+}
+
+
 $(document).ready(function() {
     setTimeout(function() {
     Morris.Area({
         element: 'morris-area-chart',
-        data: [
-            {
-                y: 0,
-                a: 0,
-                b: 0,
-            },
-            {
-                y: 20,
-                a: 0,
-                b: 3.3
-            },
-            {
-                y: 50,
-                a: 0,
-                b: 3.3,
-            },
-            {
-                y: 60,
-                a: 0,
-                b: 0,
-            },
-            {
-                y: 90,
-                a: 0,
-                b: 0
-            },
-            {
-                y: 100,
-                a: 0,
-                b: 3.3
-            },
-            {
-                y: 150,
-                a: 0,
-                b: 3.3
-            },
-            {
-                y: 160,
-                a: 0,
-                b: 0
-            },
-            {
-                y: 220,
-                a: 0,
-                b: 0
-            },
-            {
-                y: 230,
-                a: 0,
-                b: 3.3
-            },
-            {
-                y: 270,
-                a: 0,
-                b: 3.3
-            },
-            {
-                y: 280,
-                a: 0,
-                b: 0
-            },
-            {
-                y: 300,
-                a: 0,
-                b: 0, 
-            },
-            {
-                y: 320,
-                a: 0,
-                b: 3.3
-            },
-            {
-                y: 380,
-                a: 0,
-                b: 3.3
-            },
-        ],
-        xkey: 'y',
+        data: chartData,
+        xkey: 'timestamp',
         ykeys: ['a', 'b'],
         labels: ['Series A', 'Series B'],
         pointSize: 0,
